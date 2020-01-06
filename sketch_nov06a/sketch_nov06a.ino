801583968b5ffdda289cb1ce14e1184c1f3dc5da
Faith <02faith.goodman@gulllakecs.org>
2020-01-06 14:00:41 -0500

    Initial Arduino Program

---
+++ sketch_nov06a/sketch_nov06a.ino
@@ -0,0 +1,21 @@
+
+/* Programmer: Faith Goodman
+ * Date: 11.6.19
+ * Program: Blink
+ * This program will turn on an LED for a certain 
+ * amount of time, turn it off, and back on in a While Loop
+ * 
+ * Update: 11.22.19
+ * Program: Update Blink to allow the LED to fade
+ */
+
+
+
+void setup() {
+ 
+
+}
+
+void loop() {
+
+}
