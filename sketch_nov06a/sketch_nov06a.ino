4f43ec230b3fe621cd7e48e309826aca12d2ff71
Faith <02faith.goodman@gulllakecs.org>
2020-01-09 14:19:19 -0500

    1.9.20

--- sketch_nov06a/sketch_nov06a.ino
+++ sketch_nov06a/sketch_nov06a.ino
@@ -4,13 +4,27 @@
  * 
  */
 
-
+int laser = 13;
+int receiver = 2;
 
 void setup() {
- 
+  //provides output to the laser
+  pinMode(laser, OUTPUT);
 
+  //initializes serial communication at 9600 bits per second
+  Serial.begin(9600);
+
+  //Makes the reciever give input
+  pinMode(receiver, INPUT);
 }
 
 void loop() {
+  //pulls the receiver value and puts it in an integer so it can be read on the serial monitor
+  int receiverValue = digitalRead(receiver);
 
+  //prints the previous value to the serial monitor
+  Serial.println(receiverValue);
+  
+  //adds delay for stability
+  delay(1);
 }
