6b95ffabff2f33b0cbbd187202bc5442a802bd58
Luke Marchand <02luke.marchand@gulllakecs.org>
2020-01-14 14:20:44 -0500

    1.14.20

--- sketch_nov06a/sketch_nov06a.ino
+++ sketch_nov06a/sketch_nov06a.ino
@@ -4,13 +4,20 @@
  * 
  */
 
-
+int motorPin = 13; 
 
 void setup() {
- 
+  //sets the pin to output for the motor
+  pinMode(motorPin, OUTPUT);
 
 }
 
 void loop() {
+  //turns the motor on for 1 sec
+  digitalWrite(motorPin, HIGH);
+  delay(1000);
 
+  //turns the motor off for 1 sec
+  digitalWrite(motorPin, LOW);
+  delay(1000);
 }
