d4ffb84e7b62f042219d6e54f5aba724acdd6218
Luke Marchand <02luke.marchand@gulllakecs.org>
2020-01-23 13:38:43 -0500

    Initial Commit

---
+++ sketchthing/sketch_jan23a/sketch_jan23a.ino
@@ -0,0 +1,15 @@
+
+/* Programmers: Luke Marchand & Faith Goodman
+   Date: 1.6.20
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
